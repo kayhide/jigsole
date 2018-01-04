65cf5c66b154fa917765cd3b5e13bf7b94457cc3
kayhide <kayhide@gmail.com>
2018-01-04 20:32:49 +0900

    add svg.

--- frontend/src/Component.purs
+++ frontend/src/Component.purs
@@ -3,46 +3,77 @@ module Component where
 import Prelude
 
 import Data.Maybe (Maybe(..))
-
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Svg.Attributes as SA
+import Svg.Elements as SE
 
 data Query a = ToggleState a
 
-type State = { on :: Boolean }
+type Input = Unit
 
-component :: forall m. H.Component HH.HTML Query Unit Void m
-component =
+type Circle =
+  { x :: Number
+  , y :: Number
+  , r :: Number
+  }
+
+type Box =
+  { x :: Number
+  , y :: Number
+  , w :: Number
+  , h :: Number
+  }
+
+type State =
+  { circles :: Array Circle
+  , on :: Boolean
+  , viewBox :: Box
+  }
+
+initialState :: Input -> State
+initialState _ =
+  { circles: [{ x: 0.0, y: 0.0, r: 150.0 / 6.0 }]
+  , on: false
+  , viewBox: { x, y, w, h }
+  }
+  where
+    h = 150.0
+    w = 150.0
+    x = -(w / 2.0)
+    y = -(h / 2.0)
+
+ui :: forall eff. H.Component HH.HTML Query Input Void eff
+ui =
   H.component
-    { initialState: const initialState
+    { initialState
     , render
     , eval
     , receiver: const Nothing
     }
+
+render :: State -> H.ComponentHTML Query
+render state =
+  SE.svg
+  [ SA.viewBox viewBox.x viewBox.y viewBox.w viewBox.h ]
+  $ renderCircle <$> state.circles
+
   where
+    viewBox = state.viewBox
 
-  initialState :: State
-  initialState = { on: false }
-
-  render :: State -> H.ComponentHTML Query
-  render state =
-    HH.div_
-      [ HH.h1_
-          [ HH.text "Hello world!" ]
-      , HH.p_
-          [ HH.text "Why not toggle this button:" ]
-      , HH.button
-          [ HE.onClick (HE.input_ ToggleState) ]
-          [ HH.text
-              if not state.on
-              then "Don't push me"
-              else "I said don't push me!"
-          ]
+    renderCircle { x, y, r } =
+      SE.circle
+      [ SA.r r
+      , SA.fill $ Just (SA.RGB 0 (if state.on then 100 else 0) 100)
+      , HE.onClick (HE.input_ ToggleState)
       ]
 
-  eval :: Query ~> H.ComponentDSL State Query Void m
-  eval = case _ of
-    ToggleState next -> do
-      H.modify (\state -> { on: not state.on })
-      pure next
+eval :: forall eff. Query ~> H.ComponentDSL State Query Void eff
+eval (ToggleState next) = do
+  on <- H.gets _.on
+  H.modify _{ on = not on }
+  let circles = [{ x: 0.0, y: 0.0, r: 150.0 / 6.0 }]
+  H.modify _{ circles = circles }
+
+  pure next
--- frontend/src/Main.purs
+++ frontend/src/Main.purs
@@ -1,13 +1,13 @@
 module Main where
 
 import Prelude
+
+import Component (ui)
 import Control.Monad.Eff (Eff)
 import Halogen.Aff as HA
 import Halogen.VDom.Driver (runUI)
 
-import Component (component)
-
 main :: Eff (HA.HalogenEffects ()) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI component unit body
+  runUI ui unit body
